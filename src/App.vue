6dec321d9c6f5b62a000e02ca9814e756488bc1c
Your Name <you@example.com>
2021-02-07 19:43:12 +0900

    Vuejsのv-modelの学習記録として残します

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>入力欄：</span>
+    <input type="text" v-model="message">
+    <h1>{{ message }}</h1>
+    <!-- v-modelを使うことでdataの中にある
+    messageとtemplateのmessageをバインディングし、
+    textからmessageを変えることができる -->
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return{
+      message: "おはよう"
+    }
+  }
+ 
+}
+</script>
+
+
